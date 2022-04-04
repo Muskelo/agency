625df21254007f0b7e67a9e38b1d3e3b19dd3524
Muskelo <titanioman@mail.ru>
2022-04-04 10:37:32 +0300

    add test resource

--- flask_app/flaskr/resources.py
+++ flask_app/flaskr/resources.py
@@ -1,7 +1,32 @@
+from typing import List
+from pydantic import BaseModel
+from flask import current_app
 from flask_restful import Resource
-from models import UserModel
+
+from flaskr.models import UserModel
+
+
+class User(BaseModel):
+    id: int
+    login: str
+    email: str
+    password_hash: str
+
+    class Config:
+        orm_mode = True
+
+
+class Users(BaseModel):
+    users: List[User]
+
 
 class UserResource(Resource):
     def get(self):
-        users = UserModel.get_all_()
-        return {'hello': 'world'}
+        # users = UserModel.get_all_()
+        # # current_app.logger.info(users)
+
+        # return {'users' : [User.from_orm(user).dict() for user in users]}
+        users = Users(users=UserModel.get_all_())
+        # current_app.logger.info(users)
+
+        return {'users': users.dict()}
