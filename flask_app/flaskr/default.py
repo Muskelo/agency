ecd3f5da5fd7dff83b4a0ae2cb1faa6b4a0cb2fb
Muskelo <titanioman@mail.ru>
2022-04-08 18:52:05 +0300

    update default data, add roles to admin user

--- flask_app/flaskr/default.py
+++ flask_app/flaskr/default.py
@@ -1,12 +1,26 @@
 from flaskr.models import RoleModel, UserModel
 
 def init_default_data():
-    if not RoleModel.get_(False, name="admin"):
-        RoleModel.create_(name="admin")
+    # definition admin role
+    admin_role = RoleModel.get_(False, name="admin")
+    if not admin_role:
+        admin_role = RoleModel.create_(name="admin")
 
-    if not RoleModel.get_(False, name="moder"):
-        RoleModel.create_(name="moder")
+    # definition moder role
+    moder_role = RoleModel.get_(False, name="moder")
+    if not  moder_role:
+        moder_role = RoleModel.create_(name="moder")
 
-    if not UserModel.get_(False, login="admin"):
-        UserModel.create_(login="admin", email="admin@test.ru", password="admin")
+    # definition admir user
+    admin_user = UserModel.get_(False, login="admin")
+    if not  admin_user:
+        admin_user = UserModel.create_(login="admin", email="admin@test.ru", password="admin")
+
+    # set roles for admin user
+    if not admin_user in admin_role.users:
+        admin_role.users.append(admin_user)
+    if not admin_user in moder_role.users:
+        moder_role.users.append(admin_user)
+
+
 
