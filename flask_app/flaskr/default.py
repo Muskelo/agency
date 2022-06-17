5af3893c84dcd62fd1feb1b1c985dc364f25ab3a
Muskelo <titanioman@mail.ru>
2022-06-17 12:03:01 +0300

    update

--- flask_app/flaskr/default.py
+++ flask_app/flaskr/default.py
@@ -6,16 +6,16 @@ def init_default_data():
     admin_user = UserModel.get_(False, login="admin")
     if not admin_user:
         admin_user = UserModel.create_(
-            login="admin", number="1111", password="admin", role="admin")
+            login="admin", number="11111111111", password="admin", role="admin")
 
     # definition moder user
     moder_user = UserModel.get_(False, login="moder")
     if not moder_user:
         moder_user = UserModel.create_(
-            login="moder", number="2222", password="moder", role="moder")
+            login="moder", number="22222222222", password="moder", role="moder")
 
     # definition simple user
     moder_user = UserModel.get_(False, login="user")
     if not moder_user:
         moder_user = UserModel.create_(
-            login="user", number="3333", password="user")
+            login="user", number="3333333333333", password="user")
